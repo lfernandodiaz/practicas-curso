adcd5d2f7f59136cdb00415a923997b3d45ac5ef
Luis Diaz <luis.diaz@isita.com.mx>
2024-05-20 12:09:16 -0600

    Método consultar sku list y crear sku

--- node/clients/skus.ts
+++ node/clients/skus.ts
@@ -3,8 +3,18 @@ import { InstanceOptions, IOContext, JanusClient } from "@vtex/api"
 
 export class SkusClient extends JanusClient {
   private readonly routes = {
-    getSku: (account: string, refId: string) => `http://${account}.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitidbyrefid/${refId}`
+
+      getSku: (account: string, refId: string) => `http://${account}.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitidbyrefid/${refId}`,
+
+      getSkuList:(account: string, productId: string) => `http://${account}.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitByProductId/${productId}`,
+
+      createSku: (account: string) => `https://${account}.vtexcommercestable.com.br/api/catalog/pvt/stockkeepingunit`
   }
+
+
+
+
+
   constructor(context: IOContext, options?: InstanceOptions) {
     super(context, {
       ...options,
@@ -20,6 +30,25 @@ export class SkusClient extends JanusClient {
       return response
     } catch (err) {
       throw new Error(`Error fetching skuId: ${err.message}`)
+    }
+  }
+
+  public async getSkuListByProductId(productId: string): Promise<any> {
+    try {
+      const response  = await this.http.get(this.routes.getSkuList(this.context.account, productId))
+      return response
+    }
+    catch(err) {
+      throw new Error(`Error fetching SKU LIST: ${err.message}`)
+    }
+  }
+
+  public async createSku(body: any): Promise<any> {
+    try {
+      const response = await this.http.post(this.routes.createSku(this.context.account), body )
+      return response
+    } catch (err) {
+      throw new Error(`Error creating SKU: ${err.message}`)
     }
   }
 
